831870fdd55c95d60258d1e76ff402ce94549459
GoS <pup_vas2000@yahoo.com>
2020-04-04 21:20:34 +0300

    [ADD] favicon & social links

--- pages/me.tsx
+++ pages/me.tsx
@@ -13,10 +13,14 @@ const AboutPage = () => {
       <Nav page="me" />
 
       <div className="container">
-        <a href="https://www.behance.net/marikino" target="_blank" rel="noopener noreferrer" className="icon-be"><div /></a>
-        <a href="https://dribbble.com/marikino" target="_blank" rel="noopener noreferrer" className="icon-dribbble"><div /></a>
-        <a href="https://www.instagram.com/marikinoo/" target="_blank" rel="noopener noreferrer" className="icon-insta"><div /></a>
-        <a href="mailto:kinovych@gmail.com" target="_blank" rel="noopener noreferrer" className="icon-gmail"><div /></a>
+        <div className="me-photo" />
+
+        <div className="social-icons">
+          <a href="https://www.behance.net/marikino" target="_blank" rel="noopener noreferrer" className="icon-be"><div /></a>
+          <a href="https://dribbble.com/marikino" target="_blank" rel="noopener noreferrer" className="icon-dribbble"><div /></a>
+          <a href="https://www.instagram.com/marikinoo/" target="_blank" rel="noopener noreferrer" className="icon-insta"><div /></a>
+          <a href="mailto:kinovych@gmail.com" target="_blank" rel="noopener noreferrer" className="icon-gmail"><div /></a>
+        </div>
       </div>
     </>
   );
