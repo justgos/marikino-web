edc2b567f2044e7c5b837b1182d1a42f8b4c1871
GoS <pup_vas2000@yahoo.com>
2020-04-05 15:33:43 +0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a5dd3e06938066c4c10";
+/******/ 	var hotCurrentHash = "05244cc85a439fab39bb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
