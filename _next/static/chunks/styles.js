e260c9406333e3f4a184c874a63b969962a3b776
GoS <pup_vas2000@yahoo.com>
2020-04-05 19:59:52 +0300

    Updates

--- _next/static/chunks/styles.js
+++ _next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1586099188386");
+            injectCss(link, link.href.split("?")[0] + "?unix=1586105584103");
           });
       }
     }
