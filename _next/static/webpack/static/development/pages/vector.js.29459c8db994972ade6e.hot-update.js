e0fb966ebd7cf3bc7de72ff3ac5990c70fc16a1f
GoS <pup_vas2000@yahoo.com>
2020-04-06 20:22:32 +0300

    Updates

---
+++ _next/static/webpack/static/development/pages/vector.js.29459c8db994972ade6e.hot-update.js
@@ -0,0 +1,119 @@
+webpackHotUpdate("static/development/pages/vector.js",{
+
+/***/ "./pages/vector.tsx":
+/*!**************************!*\
+  !*** ./pages/vector.tsx ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ "./pages/index.scss");
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Nav__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Nav */ "./components/Nav.tsx");
+/* harmony import */ var _components_WorkTiles__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/WorkTiles */ "./components/WorkTiles.tsx");
+/* harmony import */ var _components_Header__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/Header */ "./components/Header.tsx");
+/* harmony import */ var _components_Footer__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/Footer */ "./components/Footer.tsx");
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @emotion/core */ "./node_modules/@emotion/core/dist/core.browser.esm.js");
+var _this = undefined,
+    _jsxFileName = "/mnt/c/_code/marikino-web/pages/vector.tsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+
+
+
+var VectorPage = function VectorPage() {
+  var works = [{
+    path: 'babushka.jpg',
+    size: [2000, 2000]
+  }, {
+    path: 'city_2colors.jpg',
+    size: [2000, 1500]
+  }, {
+    path: 'certificate_v1.jpg',
+    size: [1612, 906]
+  }, {
+    path: 'Visa-vsisvoi-logo-1_white.jpg',
+    size: [2048, 2048]
+  }, {
+    path: 'Visa-vsisvoi-illustration2.jpg',
+    size: [2048, 2048]
+  }, {
+    path: 'carpet_pattern.jpg',
+    size: [2664, 1998]
+  }, {
+    path: 'knygolove.jpg',
+    size: [5001, 5001]
+  }, {
+    path: 'cheek_2.jpg',
+    size: [1604, 1203]
+  }, {
+    path: 'hand.jpg',
+    size: [2000, 1500]
+  }, {
+    path: 'gifty_girl.jpg',
+    size: [1500, 1500]
+  }, {
+    path: 'workshop.jpg',
+    size: [2000, 2000]
+  }, {
+    path: 'krupa2020_illustrations_all-in-1.jpg',
+    size: [2000, 1286]
+  }, {
+    path: 'krupa_illustrations_1.jpg',
+    size: [1500, 1500]
+  }, {
+    path: 'krupa_illustrations_2.jpg',
+    size: [1500, 1500]
+  }, {
+    path: 'krupa_illustrations_3.jpg',
+    size: [1500, 1500]
+  }];
+  return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_6__["jsx"])(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_6__["jsx"])(_components_Header__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34,
+      columnNumber: 7
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_6__["jsx"])(_components_Nav__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    page: "vector",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35,
+      columnNumber: 7
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_6__["jsx"])(_components_WorkTiles__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    rootPath: "/img/vector/",
+    works: works,
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37,
+      columnNumber: 7
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_6__["jsx"])(_components_Footer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39,
+      columnNumber: 7
+    }
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (VectorPage);
+
+/***/ })
+
+})
+//# sourceMappingURL=vector.js.29459c8db994972ade6e.hot-update.js.map
