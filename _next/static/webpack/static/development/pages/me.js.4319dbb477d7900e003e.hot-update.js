fa498325ae518226dfb3e2b6201244b332f5d747
GoS <1288364+justgos@users.noreply.github.com>
2020-04-18 12:20:56 +0300

    Updates

---
+++ _next/static/webpack/static/development/pages/me.js.4319dbb477d7900e003e.hot-update.js
@@ -0,0 +1,284 @@
+webpackHotUpdate("static/development/pages/me.js",{
+
+/***/ "./pages/me.tsx":
+/*!**********************!*\
+  !*** ./pages/me.tsx ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ "./pages/index.scss");
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_Nav__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Nav */ "./components/Nav.tsx");
+/* harmony import */ var _components_Header__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/Header */ "./components/Header.tsx");
+/* harmony import */ var _emotion_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @emotion/core */ "./node_modules/@emotion/core/dist/core.browser.esm.js");
+var _this = undefined,
+    _jsxFileName = "/mnt/c/_code/marikino-web/pages/me.tsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+
+var AboutPage = function AboutPage() {
+  return Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])(_components_Header__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14,
+      columnNumber: 7
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])(_components_Nav__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    page: "me",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15,
+      columnNumber: 7
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    className: "container",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17,
+      columnNumber: 7
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    className: "me-photo",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 9
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    className: "social-icons",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20,
+      columnNumber: 9
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "https://www.behance.net/marikino",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    className: "icon-be",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21,
+      columnNumber: 11
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21,
+      columnNumber: 116
+    }
+  })), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "https://www.instagram.com/marikinoo/",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    className: "icon-insta",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22,
+      columnNumber: 11
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22,
+      columnNumber: 123
+    }
+  })), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "https://dribbble.com/marikino",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    className: "icon-dribbble",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23,
+      columnNumber: 11
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23,
+      columnNumber: 119
+    }
+  })), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "https://www.facebook.com/mari.kinovych",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    className: "icon-fb",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24,
+      columnNumber: 11
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24,
+      columnNumber: 122
+    }
+  })), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "mailto:kinovych@gmail.com",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    className: "icon-gmail",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25,
+      columnNumber: 11
+    }
+  }, Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25,
+      columnNumber: 112
+    }
+  }))), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    className: "bio",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28,
+      columnNumber: 9
+    }
+  }, "Hi! My name is Mari Kinovych,", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29,
+      columnNumber: 40
+    }
+  }), "I live and work in Kyiv, Ukraine and this website is my attempt to become a proper freelancer. ", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30,
+      columnNumber: 106
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31,
+      columnNumber: 11
+    }
+  }), "I love to be an illustrator because of this kind of freedom when you can be different with every new illustration, but also I love some boring technical tasks where you don't need to be creative.", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32,
+      columnNumber: 206
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33,
+      columnNumber: 11
+    }
+  }), "I work with different clients and tasks due to my wide experience: from lettering to illustrations for explainer videos. But my all-time favorites are illustrations for some editorial/social media purposes and books. ", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34,
+      columnNumber: 228
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 35,
+      columnNumber: 11
+    }
+  }), "Here I gather only a small part of my works but show more process and commercial cases on my\xA0", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("a", {
+    href: "https://www.behance.net/marikino",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 37,
+      columnNumber: 11
+    }
+  }, "Behance"), "\xA0page.", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 38,
+      columnNumber: 22
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39,
+      columnNumber: 11
+    }
+  }), "Besides work, I have a lot of fun with my dog, like to cook (but not every day), do yoga, plant my balcony with succulents and find pleasure in watching paintings of Old Masters. I spend too much time on social media and want to practice more analog graphics.", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40,
+      columnNumber: 270
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41,
+      columnNumber: 11
+    }
+  }), "Hope you are enjoying my illustration space!", Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42,
+      columnNumber: 55
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43,
+      columnNumber: 11
+    }
+  }), Object(_emotion_core__WEBPACK_IMPORTED_MODULE_4__["jsx"])("div", {
+    className: "heart",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 44,
+      columnNumber: 11
+    }
+  }))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (AboutPage);
+
+/***/ })
+
+})
+//# sourceMappingURL=me.js.4319dbb477d7900e003e.hot-update.js.map
