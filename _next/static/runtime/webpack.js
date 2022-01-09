ca8c71ee2ee9d20c727f326ad3a95e1ab06152ac
GoS <1288364+justgos@users.noreply.github.com>
2022-01-09 10:27:47 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "87d0f3acc391a60b9bc5";
+/******/ 	var hotCurrentHash = "312f6b9e007c1dcb38bc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
