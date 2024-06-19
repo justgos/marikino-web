e8edf9f9339e50cd0a4dadd086dc38c88ed29c73
GoS <pup_vas2000@yahoo.com>
2024-06-19 18:03:24 +0300

    fix: comic image resolution

--- pages/comic.tsx
+++ pages/comic.tsx
@@ -11,11 +11,11 @@ import Footer from '../components/Footer';
 
 const ComicPage = () => {
   const works = [
-    { path: 'Art_politics.jpg', size: [ 1024, 1024 ] },
-    { path: 'Copy of Blackout-all.jpg', size: [ 256, 1024 ] },
-    { path: 'Copy of eurovision3in1.jpg', size: [ 341, 1024 ] },
-    { path: 'Przekroj_spreadsheet.jpg', size: [ 1455, 1024 ] },
-    { path: 'TheEconomist.jpg', size: [ 339, 1024 ] },
+    { path: 'Art_politics.jpg', size: [ 2048, 2048 ] },
+    { path: 'Copy of Blackout-all.jpg', size: [ 512, 2048 ] },
+    { path: 'Copy of eurovision3in1.jpg', size: [ 683, 2048 ] },
+    { path: 'Przekroj_spreadsheet.jpg', size: [ 2909, 2048 ] },
+    { path: 'TheEconomist.jpg', size: [ 678, 2048 ] },
   ];
 
   return (
